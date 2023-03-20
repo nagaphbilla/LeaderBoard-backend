b75dece0bd00975a73558423a68b9016de92188d
Naga <nptbilla@gmail.com>
2023-03-20 16:50:03 +0530

    Added github route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
-const cors = require("cors"); 
+const cors = require("cors");
+const DomParser = require("dom-parser")
 
-
+const fetch = (...args) =>
+import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const mongoString = process.env.DATABASE_URL 
 mongoose.connect(mongoString);
@@ -22,6 +24,21 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/users',userRoute);
 app.use('/api/data',dataRoute);
+
+// fetch("https://github.com/users/github/contributions?to=2023-12-31")
+//         .then(data => data.text())
+//         .then(res => {
+//             var parser = new DomParser();
+//             // console.log(res);
+//             var dom = parser.parseFromString(res)
+//             if(res == "Not Found") {
+//                 console.log(res);
+//             }
+//             else {
+//             var element = dom.getElementsByTagName('h2')[0].textContent
+//             console.log(parseInt(element, 10))
+//             }
+//         })
    
 
 const PORT = process.env.PORT || 5000;
--- routes/data.js
+++ routes/data.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const User = require("../model/userModel")
+const DomParser = require("dom-parser")
 const fetch = (...args) =>
 import('node-fetch').then(({default: fetch}) => fetch(...args));
 
@@ -23,6 +24,56 @@ router.get('/leetcode', (req, res) => {
                 values[i]["name"] = urls[i]["name"]
             }
             res.status(200).json(values)
+        })
+    })
+})
+router.get('/github', (req, res) => {
+    var urls = [];
+    User.find()
+    .then(users => {
+        console.log(users.length);
+        users.map(async user => {
+            let id = user["github"]
+            console.log(id);
+            let details = {"name" : user["name"], "username" : user["github"]}
+            if(id == "") {
+                id = "github"
+            }
+            const url = `https://github.com/users/${id}/contributions?to=2023-12-31`
+            details["repos"] = 0
+            details["url"] = url
+            urls.push(details)
+        })
+    })
+    .then(() => {
+        Promise.all(urls.map(async url => {
+            if(url["username"] != "") {
+                const data = await fetch("https://api.github.com/users/Vedavyas11062004")
+                const res = await data.json()
+                url["repos"] = res["public_repos"]
+            }
+            const data = await fetch(url["url"])
+            return data.text()
+        }))
+        .then(values => {
+            console.log("URLS - ", urls.length);
+            console.log("Values - ", values.length);
+            var result = []
+            var parser = new DomParser();
+            for(var i=0;i<values.length;i++) {
+                console.log(urls[i]);
+                let details = {"name" : urls[i]["name"], "username" : urls[i]["username"], "repos" : urls[i]["repos"]}
+                if(values[i] == "Not Found") {
+                    details["contributions"] = null
+                }
+                else {
+                    var dom = parser.parseFromString(values[i])
+                    var element = dom.getElementsByTagName('h2')[0].textContent
+                    details["contributions"] = parseInt(element, 10)
+                }
+                result.push(details)
+            }
+            res.status(200).json(result)
         })
     })
 })
