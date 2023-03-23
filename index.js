095975af0b7f7f8ffc4c0f3f1543c921a20f6be2
Naga <nptbilla@gmail.com>
2023-03-23 15:39:40 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const app = express();
 const cors = require("cors");
-const DomParser = require("dom-parser")
-
-const fetch = (...args) =>
-import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-const mongoString = process.env.DATABASE_URL 
-mongoose.connect(mongoString);
 const userRoute= require('./routes/user');
 const dataRoute= require('./routes/data');
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.status(200).send("Server is running properly")
+})
+
+app.use('/api/users',userRoute);
+app.use('/api/data',dataRoute);
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DATABASE_URL);
 const database = mongoose.connection
 database.on('error',(error)=>{
     console.log(error);
@@ -19,33 +28,8 @@ database.once('connected',()=>{
     console.log('Database Connected');
 })
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use('/api/users',userRoute);
-app.use('/api/data',dataRoute);
-
-// fetch("https://github.com/users/github/contributions?to=2023-12-31")
-//         .then(data => data.text())
-//         .then(res => {
-//             var parser = new DomParser();
-//             // console.log(res);
-//             var dom = parser.parseFromString(res)
-//             if(res == "Not Found") {
-//                 console.log(res);
-//             }
-//             else {
-//             var element = dom.getElementsByTagName('h2')[0].textContent
-//             console.log(parseInt(element, 10))
-//             }
-//         })
-   
-
-const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT,()=>{
-    console.log(`Server started on  ${PORT}`)
+    console.log(`Server started on ${PORT}`)
 })
 
 
